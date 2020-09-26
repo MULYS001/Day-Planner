eb1d256ddefa44a3edb7cf933867604c6cabfb4e
MULYS001 <mulys001@gmail.com>
2020-09-26 17:28:48 +0800

    Initial commit and changes

---
+++ script.js
@@ -0,0 +1,59 @@
+// Showing the date at the top of the page
+
+setInterval(function () {
+    var today = moment();
+    $("#currentDay").text(today.format("dddd, MMMM YYYY - h:mm:ss a"));
+}, 1000)
+
+//find out current time
+
+let timeNow = moment();
+
+var workHours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var todaysDateEl = $("#currentDay");
+
+
+// function to clone timeblocks from single timeblock in html
+
+function setStatus() {
+
+    // for loop to create the row
+    for (let i = 1; i < workHours.length; i++) {
+        let newTimeBlock = $("#9am").clone();
+        newTimeBlock.attr("id", workHours[i]);
+        newTimeBlock.children(".hour").text(workHours[i]);
+        newTimeBlock.appendTo(".container");
+    }
+}
+
+// funtion to classify rows for past, present or future accordingly
+function classifyTime() {
+
+    let timeOfDay = moment().format("ha");
+    let allTimeBlockEl = $(".time-block");
+
+    // looping through all time block elements to find each timeblock id and assign the hour
+    for (let i = 0; i < allTimeBlockEl.length; i++) {
+
+        let timeBlock = $(allTimeBlockEl[i]);
+        let timeBlockId = timeBlock.attr("id");
+        let timeBlockTextarea = timeBlock.children(".schedule");
+
+        // Setting class to present if timeblock is same as current hour
+        if (timeBlockId === timeOfDay) {
+            timeBlockTextarea.addClass("present");
+
+            // setting class to past if before current hour
+        } else if (moment(timeBlockId, "ha").isBefore()) {
+            timeBlockTextarea.addClass("past");
+
+            // setting class to future if after current hour
+        } else if (moment(timeBlockId, "ha").isAfter()) {
+            timeBlockTextarea.addClass("future");
+        }
+    }
+}
+
+setStatus()
+classifyTime()
+
